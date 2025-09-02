b16a0c955fd8bc1f4f90d80e010b453298b389e4
Rok Novosel <rok@poolside.ai>
2025-09-02 12:29:19 +0200

    Modernize project.

--- cmd/pt/main.go
+++ cmd/pt/main.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"os"
-	"runtime"
 
 	pt "github.com/monochromegane/the_platinum_searcher"
 )
-
-func init() {
-	if cpu := runtime.NumCPU(); cpu == 1 {
-		runtime.GOMAXPROCS(2)
-	} else {
-		runtime.GOMAXPROCS(cpu)
-	}
-}
 
 func main() {
 	pt := pt.PlatinumSearcher{Out: os.Stdout, Err: os.Stderr}
